632054fcc3fd9e12d5e3e41ed0d2763d957f7e57
Rak2142 <214541226+Rak2142@users.noreply.github.com>
2026-03-26 11:09:36 -0500

    Added andrew line

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 	System.out.printf("Alex here!");
+        System.out.println("Andrew is here");
         
 
     }
